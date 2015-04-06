83d4f7544044b3eff84e5da713ed0f09677df603
cjhill <cjhill0216@gmail.com>
2015-04-06 12:11:43 -0400

    4/6/15

--- footer.php
+++ footer.php
@@ -2,5 +2,7 @@
 	<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
 	<!-- Bootstrap JS CDN -->
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+	<!-- Modernizr CDN -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
 	</body>
 </html>
